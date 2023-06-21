8e2cf50457228768214549f43c870ff6d3ba6c9d
zkapic <kapiczahid@gmail.com>
2023-06-21 22:52:53 +0200

    Vercel

--- catering/build_files.sh
+++ catering/build_files.sh
@@ -1,3 +1,15 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/Scripts/activate
+
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
